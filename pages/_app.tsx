0e034cd112d8eb8b2fdb2f0479228be27df59305
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2022-11-25 10:41:38 +0530

    Add theme and MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+// import { ThemeProvider, CssBaseline  } from "@mui/material";
+import ThemeProvider from "../theme";
+import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+import PropTypes from 'prop-types';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }: AppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
